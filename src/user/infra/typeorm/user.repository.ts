71cb8525af66384306d60fd886927f0337eeec75
Mateus Sousa <sousa.programador@gmail.com>
2023-08-02 08:57:58 -0300

    refactor generals

--- src/user/infra/typeorm/user.repository.ts
+++ src/user/infra/typeorm/user.repository.ts
@@ -8,6 +8,8 @@ import {
 import { UserModelMapper } from './user-mapper';
 
 export class UserRepository implements UserRepositoryContract.Repository {
+  sortableFields: string[] = ['name', 'email'];
+
   constructor(private userModel: Repository<UserEntity>) {}
 
   search(
@@ -15,24 +17,29 @@ export class UserRepository implements UserRepositoryContract.Repository {
   ): Promise<UserRepositoryContract.SearchResult> {
     throw new Error('Method not implemented.');
   }
+
   insert(entity: User): Promise<void> {
     throw new Error('Method not implemented.');
   }
+
   bulkInsert(entities: User[]): Promise<void> {
     throw new Error('Method not implemented.');
   }
+
   async findById(id: string | UserId): Promise<User> {
     const user = await this.userModel.findOne({ where: { id: `${id}` } });
     return UserModelMapper.toEntity(user);
   }
+
   async findAll(): Promise<any> {
     return await this.userModel.find();
   }
-  update(entity: User): Promise<void> {
+
+  async update(entity: User): Promise<void> {
     throw new Error('Method not implemented.');
   }
-  delete(id: string | UserId): Promise<void> {
-    throw new Error('Method not implemented.');
+
+  async delete(id: string | UserId): Promise<void> {
+    await this.userModel.softDelete({ id: `${id}` });
   }
-  sortableFields: string[] = ['name', 'email'];
 }
