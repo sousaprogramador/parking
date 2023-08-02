823dd99ca07083b1367d8ea01266635875d1f715
Mateus Sousa <sousa.programador@gmail.com>
2023-08-02 08:30:44 -0300

    example erros

---
+++ src/user/application/use-cases/get-user.use-case.int.spec.ts
@@ -0,0 +1,21 @@
+import { GetUserUseCase } from './get-user.use-case';
+import { UserRepository, UserEntity } from '../../infra/typeorm';
+import { DataSource } from 'typeorm';
+import { databaseProviders } from '../../../database/database.providers';
+
+describe('GetUserUseCase Integration Tests', () => {
+  let useCase: GetUserUseCase.UseCase;
+  let repository: UserRepository;
+  let dataSource: DataSource;
+
+  beforeEach(async () => {
+    dataSource = await databaseProviders[0].useFactory();
+    repository = new UserRepository(dataSource.getRepository(UserEntity));
+    useCase = new GetUserUseCase.UseCase(repository);
+  });
+
+  it('should create a user', async () => {
+    const all = await repository.findAll();
+    console.log('all', all);
+  });
+});
